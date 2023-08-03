365c3353db9501c4dce9d617aabd720832ddc496
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-08-03 12:38:38 +0300

    Alter test_get_async_session

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,7 @@
 from typing import AsyncGenerator
 
 import pytest
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from .conftest import get_async_session
 
@@ -8,4 +9,6 @@ from .conftest import get_async_session
 @pytest.mark.anyio
 async def test_get_async_session():
     assert isinstance(get_async_session(), AsyncGenerator)
+    asession = await get_async_session().__anext__()
+    assert isinstance(asession, AsyncSession)
 
