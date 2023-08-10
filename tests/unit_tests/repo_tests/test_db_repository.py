278b1dd089bbf3a673db896ec2ca71e616b559f5
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-08-10 18:23:48 +0300

    Added tests for menu, submenu, dish repos

--- tests/unit_tests/repo_tests/test_db_repository.py
+++ tests/unit_tests/repo_tests/test_db_repository.py
@@ -1,6 +1,11 @@
 import pytest
 
-from app.repositories.db_repository import CRUDRepository, MenuRepository
+from app.repositories.db_repository import (
+    CRUDRepository,
+    DishRepository,
+    MenuRepository,
+    SubmenuRepository,
+)
 from tests.fixtures.data import Model
 from tests.utils import get_method
 
@@ -21,8 +26,52 @@ class TestCRUDRepository:
 
 
 class TestMenuRepository:
-    menu_repo_db: MenuRepository
+    NOT_FOUND = 'menu not found'
+    OBJECT_ALREADY_EXISTS = 'Меню с таким заголовком уже существует.'
+    repo_db: MenuRepository
 
     @pytest.fixture
     def init(self, get_test_session):
-        self.menu_repo_db = MenuRepository(get_test_session)
+        self.repo_db = MenuRepository(get_test_session)
+
+    def test_messages(self, init):
+        assert self.NOT_FOUND == self.repo_db.NOT_FOUND
+        assert self.OBJECT_ALREADY_EXISTS == self.repo_db.OBJECT_ALREADY_EXISTS
+
+
+class TestSubmenuRepository:
+    NOT_FOUND = 'submenu not found'
+    OBJECT_ALREADY_EXISTS = 'Подменю с таким заголовком уже существует.'
+    repo_db: SubmenuRepository
+
+    @pytest.fixture
+    def init(self, get_test_session):
+        self.repo_db = SubmenuRepository(get_test_session)
+
+    def test_messages(self, init):
+        assert self.NOT_FOUND == self.repo_db.NOT_FOUND
+        assert self.OBJECT_ALREADY_EXISTS == self.repo_db.OBJECT_ALREADY_EXISTS
+
+    def test_perform_create_method(self, init):
+        create_data = {}
+        self.repo_db.perform_create(create_data, 'menu.id')
+        assert create_data['menu_id'] == 'menu.id'
+
+
+class TestDishRepository:
+    NOT_FOUND = 'dish not found'
+    OBJECT_ALREADY_EXISTS = 'Блюдо с таким заголовком уже существует.'
+    repo_db: DishRepository
+
+    @pytest.fixture
+    def init(self, get_test_session):
+        self.repo_db = DishRepository(get_test_session)
+
+    def test_messages(self, init):
+        assert self.NOT_FOUND == self.repo_db.NOT_FOUND
+        assert self.OBJECT_ALREADY_EXISTS == self.repo_db.OBJECT_ALREADY_EXISTS
+
+    def test_perform_create_method(self, init):
+        create_data = {}
+        self.repo_db.perform_create(create_data, 'submenu.id')
+        assert create_data['submenu_id'] == 'submenu.id'
