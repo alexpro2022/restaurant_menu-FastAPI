2e6dc809e7f6377de455a1de78755c3d2c2e9250
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-08-14 20:02:54 +0300

    Removed Docker.celery

--- app/api/endpoints/synch.py
+++ app/api/endpoints/synch.py
@@ -1,20 +1,11 @@
 from fastapi import APIRouter
 
 from app.core import settings
-from app.tasks import _synchronize, synchronize
+from app.tasks import _synchronize
 
 router = APIRouter(prefix=f'{settings.URL_PREFIX}synchronize', tags=['Database'])
 
 SUMMARY = 'Активирует процесс синхронизации admin-файла и БД.'
-
-
-@router.get(
-    '',
-    summary=SUMMARY,
-    description=(f'{settings.ALL_USERS} {SUMMARY}'))
-async def synch():
-    # session: AsyncSession = Depends(get_async_session)) -> None:
-    synchronize.delay()
 
 
 @router.get(
